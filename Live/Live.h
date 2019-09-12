2e8cb750717712b956306e37247bb663efd0c80c
Hexagons <anton.heestand@gmail.com>
2019-09-12 10:11:50 +0200

    Initial Commit

---
+++ Live/Live.h
@@ -0,0 +1,19 @@
+//
+//  Live.h
+//  Live
+//
+//  Created by Anton Heestand on 2019-09-12.
+//  Copyright © 2019 Hexagons. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Live.
+FOUNDATION_EXPORT double LiveVersionNumber;
+
+//! Project version string for Live.
+FOUNDATION_EXPORT const unsigned char LiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Live/PublicHeader.h>
+
+
